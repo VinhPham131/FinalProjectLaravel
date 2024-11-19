13e9cba46b6fd526010e919fb43b1327adc9cc79
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-11-19 11:42:19 +0700

    generate ui test

--- final_laravel/resources/views/layouts/app.blade.php
+++ final_laravel/resources/views/layouts/app.blade.php
@@ -1,36 +1,59 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="LBJ Jewlery Luxury"/>
+    <title>LBJ</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="manifest" href="/site.webmanifest">
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap");
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        body {
+            font-family: "DM Sans", sans-serif;
+        }
+        
+        #fontgucci {
+            font-family: "Playfair Display", serif;
+        }
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        .col {
+            width: 92.5%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+   
+</head>
+<body>
+    <x-header />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    @yield('content')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <x-footer />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J7QBN2WL4D"></script>
+<script>
+    // OLD code is: src/scripts/gganalytics.js
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'G-J7QBN2WL4D');
+</script>
+
+<!-- Additional scripts customized for specific screens -->
+@yield('script')
+
+</body>
 </html>
--- final_laravel/tailwind.config.js
+++ final_laravel/tailwind.config.js
@@ -1,18 +1,31 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+const forms = require('@tailwindcss/forms');
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
 
     theme: {
+        screens: {
+            phone: '320px',
+            tablet: '640px',
+            laptop: '1400px',
+            desktop: '1920px',
+            sm: '576px',
+            md: '960px',
+            lg: '1440px',
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                roboto: ['Roboto', 'sans-serif'],
+            },
+            width: {
+                'calc-1/2': 'calc(50% - 1rem)',
             },
         },
     },
