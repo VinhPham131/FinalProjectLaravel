ca530519270a6b57e52709d3324eb116a177c1e0
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-11-25 21:19:24 +0700

    Fix search function

--- final_laravel/app/Http/Controllers/ShopController.php
+++ final_laravel/app/Http/Controllers/ShopController.php
@@ -7,9 +7,24 @@ use App\Models\Product;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with('images')->get();
+        $search = $request->input('search');
+
+        $products = Product::with('images')
+            ->when($search, function ($query, $search) {
+                $query->where('name', 'LIKE', '%' . $search . '%');
+            })
+            ->get();
+
+        // Check if the request is an AJAX call
+        if ($request->ajax()) {
+            return view('partials.products', compact('products'))->render();
+        }
+
         return view('shop', compact('products'));
     }
+
+
+    
 }
