0b26d1ac9f3c8d38683f2c1379b1594c670c905e
comeheretnt <minh.pham220208@vnuk.edu.vn>
2024-12-14 16:03:38 +0700

    change detailController

--- final_laravel/app/Http/Controllers/DetailController.php
+++ final_laravel/app/Http/Controllers/DetailController.php
@@ -12,22 +12,26 @@ class DetailController extends Controller
         $product = Product::where('slug', $slug)->firstOrFail();
 
         // Calculate sale-related attributes for the main product
-        $this->calculateSaleAttributes($product); // TODO: SHOULD IMPROVE THIS
+        $this->calculateSaleAttributes($product);
 
         $related_products = Product::with('images')
             ->select([
-                'products.*',
+                'products.id',
+                'products.slug',
+                'products.name',
+                'products.price',
+                'products.category_id',
                 DB::raw('MAX(sales.percentage) as highest_sale'),
             ])
             ->join('product_categories', 'product_categories.id', '=', 'products.category_id')
             ->join('sales', 'sales.name', '=', 'product_categories.name')
-            ->groupBy('products.id')
+            ->groupBy('products.id', 'products.slug', 'products.name', 'products.price', 'products.category_id')
             ->get()
             ->each(function (Product $product) {
                 if ($product->highest_sale) {
                     $product->discounted_price = $product->highest_sale
-                    ? $product->price * (1 - $product->highest_sale / 100)
-                    : $product->price;
+                        ? $product->price * (1 - $product->highest_sale / 100)
+                        : $product->price;
                 } else {
                     $product->discounted_price = null;
                 }
