2dc36f9c89204ebd1524cfa8fc416bbe1d35f49e
ngoinhaoto <dinhchinhle487@gmail.com>
2024-12-16 23:22:29 +0700

    fixing performance issue (duplicates query) in shop controller

--- final_laravel/app/Http/Controllers/ShopController.php
+++ final_laravel/app/Http/Controllers/ShopController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\ProductCategoryController;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class ShopController extends Controller
 {
@@ -21,6 +23,10 @@ class ShopController extends Controller
         $search = $request->input('search');
         $sort = $request->input('sort'); // Retrieve the sort parameter
 
+        $categories = Cache::remember('categories', 60, function () {
+            return $this->productCategoryController->index();; // Retrieve directly from the database or cache
+        });
+
         $products = Product::with(['images', 'category', 'collection'])
             ->when($search, function ($query, $search) {
                 $query->where('name', 'LIKE', '%' . $search . '%');
@@ -35,14 +41,14 @@ class ShopController extends Controller
                         break;
                 }
             })
-            ->get()
-            ->map(function ($product) {
-                $product->highest_sale = $product->highestSale();
-                $product->discounted_price = $product->salePrice();
-                return $product;
-            });
-
-        $categories = $this->productCategoryController->index();
+            ->select('products.*', DB::raw('MAX(sales.percentage) as highest_sale'), DB::raw('MAX(sales.percentage) * products.price / 100 as discounted_price'))
+            ->join('product_categories', 'product_categories.id', '=', 'products.category_id') // Join with categories
+            ->leftJoin('sales', function ($join) {
+                $join->on('sales.name', '=', 'product_categories.name') // Match sale's name to category name
+                    ->where('sales.sale_target', '=', 'category'); // Only for categories (or collections, as needed)
+            })
+            ->groupBy('products.id')
+            ->get();
 
         if ($request->ajax()) {
             return view('partials.products', compact('products'))->render();
