cd8031afc97bac7c86823c61624c1f58dc1d5c6f
VOIDkuugeki <voidkuugeki@gmail.com>
2024-12-22 01:45:33 +0700

    implement remember me func into logout of fortify

--- final_laravel/routes/auth.php
+++ final_laravel/routes/auth.php
@@ -1,16 +1,21 @@
 <?php
 
-use App\Http\Controllers\verifyRegistrationController;
+use App\Http\Controllers\VerifyRegistrationController;
+use App\Http\Controllers\Auth\LogoutUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
 });
 
-Route::get('/email/verify/{id}/{hash}', [verifyRegistrationController::class, '__invoke'])
+Route::get('/email/verify/{id}/{hash}', [VerifyRegistrationController::class, '__invoke'])
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
 
 Route::get('/login', function () {
     return redirect()->route('home');
 })->name('login');
+
+Route::post('/logout', [LogoutUserController::class, 'destroy'])
+    ->middleware([config('fortify.auth_middleware', 'auth') . ':' . config('fortify.guard')])
+    ->name('logout');
