e447898c0c10d8981289e0fd7dd37de436dbbf53
Vinh Phạm <vinhpham@192.168.1.8>
2024-12-24 13:16:50 +0700

    Update Cart

--- final_laravel/resources/views/livewire/products.blade.php
+++ final_laravel/resources/views/livewire/products.blade.php
@@ -29,14 +29,14 @@
         <div class="text-center mt-8">
             @if (count($products) < $totalProducts)
                 <button wire:click="showMore" type="button"
-                    class="text-white bg-[#A18A68] hover:bg-[#8C7A53] focus:ring-4 focus:ring-[#8C7A53] transition-all duration-300 transform hover:scale-80 font-medium rounded-lg text-lg px-6 py-3 mb-4 shadow-lg hover:shadow-2xl focus:outline-none">
+                    class="text-white bg-[#A18A68] hover:bg-[#8C7A53] focus:ring-4 focus:ring-[#8C7A53] transition-all duration-300 transform hover:scale-80 font-medium rounded-lg text-lg px-6 py-1 mb-4 shadow-lg hover:shadow-2xl focus:outline-none">
                     Show More
                 </button>
             @endif
 
             @if ($limit > 8)
                 <button wire:click="showLess" type="button"
-                    class="text-white bg-gray-700 hover:bg-[#A18A68] focus:ring-4 focus:ring-[#8C7A53] transition-all duration-300 transform hover:scale-80 font-medium rounded-lg text-lg px-6 py-3 mb-4 shadow-lg hover:shadow-2xl focus:outline-none">
+                    class="text-white bg-gray-700 hover:bg-[#A18A68] focus:ring-4 focus:ring-[#8C7A53] transition-all duration-300 transform hover:scale-80 font-medium rounded-lg text-lg px-6 py-1 mb-4 shadow-lg hover:shadow-2xl focus:outline-none">
                     Show Less
                 </button>
             @endif
