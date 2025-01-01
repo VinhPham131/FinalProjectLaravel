f4462a91b755d99906d2a961fc4b85b3d43afc21
rither <dinhchinhle487@gmail.com>
2025-01-01 19:12:26 +0700

    changing api token behaviour for admin only. adding collection api

---
+++ final_laravel/app/Http/Controllers/API/CollectionController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CollectionController extends Controller
+{
+
+    /**
+     * Display a listing of collection.
+     * @unauthenticated
+     */
+    public function index()
+    {
+        $collections = Collection::all();
+        return response()->json($collections);
+    }
+
+    /**
+     * Display a particular collection.
+     * @unauthenticated
+     */
+    public function show($id)
+    {
+        $collection = Collection::find($id);
+
+        if (!$collection) {
+            return response()->json(['message' => 'Collection not found'], 404);
+        }
+
+        return response()->json($collection);
+    }
+
+    /**
+     * Creating a new collection.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $collection = Collection::create($request->all());
+
+        return response()->json($collection, 201);
+    }
+
+    /**
+     * Updating a collection.
+     */
+    public function update(Request $request, $id)
+    {
+        $collection = Collection::find($id);
+
+        if (!$collection) {
+            return response()->json(['message' => 'Collection not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $collection->update($request->all());
+
+        return response()->json($collection);
+    }
+
+    /**
+     * Deleting a collection.
+     */
+    public function destroy($id)
+    {
+        $collection = Collection::find($id);
+
+        if (!$collection) {
+            return response()->json(['message' => 'Collection not found'], 404);
+        }
+
+        $collection->delete();
+
+        return response()->json(['message' => 'Collection deleted successfully']);
+    }
+}
--- final_laravel/routes/api.php
+++ final_laravel/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CollectionController;
 use App\Http\Controllers\API\ProductCategoryController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Middleware\EnsureUserIsAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,15 +12,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', EnsureUserIsAdmin::class])->group(function () {
     Route::post('/category', [ProductCategoryController::class, 'store']);
     Route::put('/category/{id}', [ProductCategoryController::class, 'update']);
     Route::delete('/category/{id}', [ProductCategoryController::class, 'destroy']);
     Route::post('/product', [ProductController::class, 'store']);
     Route::put('/product/{id}', [ProductController::class, 'update']);
     Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+    Route::post('/collection', [CollectionController::class, 'store']);
+    Route::put('/collection/{id}', [CollectionController::class, 'update']);
+    Route::delete('/collection/{id}', [CollectionController::class, 'destroy']);
 });
 
 Route::apiResource('/category', ProductCategoryController::class)->only(['index', 'show']);
 Route::apiResource('/product', ProductController::class)->only(['index', 'show']);
+Route::apiResource('/collection', CollectionController::class)->only(['index', 'show']);
 Route::post('/login', [AuthController::class, 'login']);
